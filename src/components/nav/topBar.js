031bd36839e5ea9042ca43a82ff2c5a8b476080d
cytescale <nikhil@cytescale.com>
2022-08-31 19:09:40 +0530

    commit

--- src/components/nav/topBar.js
+++ src/components/nav/topBar.js
@@ -4,31 +4,50 @@ import Link from "@mui/material/Link";
 import Stack from "@mui/material/Stack";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import Typography from "@mui/material/Typography";
+import styled from "styled-components";
+
+const MTopBarS = styled.div`
+  width: 100%;
+  height: 52px;
+  position: relative;
+  background-color: #fff;
+  box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.04);
+  display: inline-flex;
+  z-index: 30;
+  color: #fff;
+`;
+
+const MTopBarRightS = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-left: 22px;
+`;
+
+const MBreadCurr = styled.div`
+  font-size: 14px;
+  color: #0057ff;
+`;
 
 const TopBar = (props) => {
   const breadcrumbs = [
     <Link underline="hover" key="1" color="inherit" href="/">
-      MUI
+      Products
     </Link>,
-    <Link underline="hover" key="2" color="inherit" href="/">
-      Core
-    </Link>,
-    <Typography key="3" color="text.primary">
-      Breadcrumb
-    </Typography>,
+    <MBreadCurr key="2" color="#0057FF">
+      Add Product
+    </MBreadCurr>,
   ];
   return (
     <>
-      <div className="app-topbar-main-cont">
-        <div className="topbar-page-lab-cont">
-          <Breadcrumbs
-            separator={<i class="ri-arrow-right-s-line"></i>}
-            aria-label="breadcrumb"
-          >
+      <MTopBarS>
+        <MTopBarRightS>
+          <Breadcrumbs separator={<i class="ri-arrow-right-s-line" />}>
             {breadcrumbs}
           </Breadcrumbs>
-        </div>
-      </div>
+        </MTopBarRightS>
+      </MTopBarS>
     </>
   );
 };
