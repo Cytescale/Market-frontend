389b10fe2714e9f3abb7feba0d62b9a66500af23
cytescale <nikhil@cytescale.com>
2022-10-27 20:08:47 +0530

    earning page commit

--- src/components/pages/earnings/earnings.js
+++ src/components/pages/earnings/earnings.js
@@ -65,12 +65,21 @@ const EarningChart = (props)=>{
 
 const EarningContri = (props) =>{
 const[percent,setpercent] = useState(25);
-
+var colorArray = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+		  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+		  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+		  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+		  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+		  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+		  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+		  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+		  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+		  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
   return(
     <div>
          <VictoryPie
             cornerRadius={({ datum }) => datum.y * 5}
-            colorScale={["tomato", "orange", "gold", "cyan", "navy" ]}
+            colorScale={colorArray}
 
             data={[
               { x: "Product1", y: 35 },
@@ -124,8 +133,8 @@ const EarningBase = (props) => {
               
               </div>
           </div>
-        <div className="app-earn-mid-slab-cont">
-                  <div className="app-earn-card-main-cont">
+        <div className="app-earn-mid-slab-cont mid-slab-justi">
+                  <div className="app-earn-card-main-cont ">
                     <div className="app-earn-card-row-cont">
                       <EarningCardCont />
                       <EarningCardCont />  
@@ -136,7 +145,7 @@ const EarningBase = (props) => {
                     </div>
                   </div>
               <div className="app-earn-chart-main-cont">  
-              <div className="app-earn-seg-lab-cont">Earnings Chart</div>
+              <div className="app-earn-seg-lab-cont"><i class="ri-line-chart-line"></i>Earnings Chart</div>
                 <div className="app-earn-seg-body">
                   <div className="app-earn-chart-cont">
                       <EarningChart/>
@@ -146,7 +155,7 @@ const EarningBase = (props) => {
         </div>
         <div className="app-earn-bottom-cont">
             <div className="app-earn-bottom-left-cont">
-              <div className="app-earn-seg-lab-cont">Earnings by products</div>
+              <div className="app-earn-seg-lab-cont"><i class="ri-table-line"></i>Earnings by products</div>
               <div className="app-earn-seg-body">
                   <div className="app-earn-tabl-main-cont">
                     <EarningTable />
@@ -154,7 +163,7 @@ const EarningBase = (props) => {
               </div>
             </div>
             <div className="app-earn-bottom-right-cont">
-                <div className="app-earn-seg-lab-cont seg-lab-ver-2">Product Contribution</div>
+                <div className="app-earn-seg-lab-cont seg-lab-ver-2"><i class="ri-pie-chart-line"></i>Product Contribution</div>
                 <div className="app-earn-seg-body">
                     <EarningContri/>
                 </div>
